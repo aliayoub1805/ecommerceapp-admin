dd22581d576d1d31e25017ad028af228ccef11cd
aliayoub1805 <aliayoub_1987@windowslive.com>
2024-09-12 11:49:54 +0300

    Add admin app bar

---
+++ lib/views/screens/main_screen.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_admin_scaffold/admin_scaffold.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AdminScaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.yellowAccent.shade700,
+        title: Text('Management'),
+      ),
+      sideBar: SideBar(items: [
+        AdminMenuItem(title: 'Categories',icon: Icons.category, route: '/'),
+        AdminMenuItem(title: 'Categories',icon: Icons.category, route: '/'),
+        AdminMenuItem(title: 'Categories',icon: Icons.category, route: '/'),
+        AdminMenuItem(title: 'Categories',icon: Icons.category, route: '/'),
+      ],
+        selectedRoute: '',
+      ),
+      body: Text('Dashboard'), );
+  }
+}
