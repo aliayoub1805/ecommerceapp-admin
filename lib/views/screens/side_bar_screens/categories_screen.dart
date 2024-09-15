e048992e43372e0b936e79e3230a9abdbcc08c4d
aliayoub1805 <aliayoub_1987@windowslive.com>
2024-09-15 12:51:49 +0300

    update files : add category_widget.dart and update categories_screen.dart

--- lib/views/screens/side_bar_screens/categories_screen.dart
+++ lib/views/screens/side_bar_screens/categories_screen.dart
@@ -1,7 +1,11 @@
 
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommer_test_mode_admin/views/screens/side_bar_screens/widgets/category_widget.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:typed_data'; // لاستخدام Uint8List
 
@@ -16,9 +20,13 @@ class CategoriesScreen extends StatefulWidget {
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final ImagePicker _picker = ImagePicker();
   String?   fileName;
+  String? categoryName;
+  bool _isUploading = false;
+  //final TextEditingController _categoryController = TextEditingController();
   //Uint8List? _image;
  // لتخزين بيانات الصورة
   dynamic _image;
@@ -37,11 +45,48 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
     }
   }
 
-  uploadCategory(){
-    if(_formKey.currentState!.validate()){
-      print("Good");
-    }
-    else {
+  _uploadCategoryBannerToStorage(dynamic image) async{
+      Reference ref = _storage.ref().child('categoryImages').child(fileName!);
+      UploadTask uploadTask = ref.putData(image);
+      TaskSnapshot snapshot = await uploadTask;
+      String downloadUrl = await snapshot.ref.getDownloadURL();
+      return downloadUrl;
+  }
+  uploadCategory()async{
+    if (_formKey.currentState!.validate()) {
+      EasyLoading.show();
+      try {
+        String imageUrl = await _uploadCategoryBannerToStorage(_image);
+        await _firestore.collection('categories').doc(fileName).set({
+          'image': imageUrl,
+          'categoryName': categoryName,
+        }).whenComplete(() {
+          setState(() {
+            _image = null;
+            //_categoryController.clear(); // إعادة تعيين TextEditingController
+            _formKey.currentState!.reset();
+          });
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Category uploaded successfully'),
+              backgroundColor: Colors.green,
+            ),
+          );
+        });
+      } catch (e) {
+
+        print("Error uploading category: $e");
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Error uploading category'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      }
+      finally{
+        EasyLoading.dismiss();
+      }
+    } else {
       print("Bad");
     }
   }
@@ -116,6 +161,10 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
                   child: SizedBox(
                     width: 200,
                     child: TextFormField(
+                      //controller: _categoryController,
+                      onChanged: (value){
+                        categoryName = value;
+                      },
                       validator: (value){
                         if(value!.isEmpty){
                           return "Category name must not be empty";
@@ -153,6 +202,18 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
                 ),
               ],
             ),
+            Divider(color: Colors.grey,),
+            Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Categories',
+                style: TextStyle(
+                    fontSize: 26 ,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            CategoryWidget(),
           ],
         ),
       ),
